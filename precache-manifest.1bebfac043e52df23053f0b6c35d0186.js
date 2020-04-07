b083f863f05ff24421dcfe38b1088dcd947dedc2
adzinets <adzinetskatsiaryna@gmail.com>
2020-04-07 21:35:24 +0300

    Updates

---
+++ precache-manifest.1bebfac043e52df23053f0b6c35d0186.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b4b7650bd8f9d01dfca1dee10408e9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "85ccf0fe57819c80bd96",
+    "url": "/portfolio/static/css/main.5823c45b.chunk.css"
+  },
+  {
+    "revision": "eb3b79343ce48086ef2f",
+    "url": "/portfolio/static/js/2.b8ea2ce4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.b8ea2ce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85ccf0fe57819c80bd96",
+    "url": "/portfolio/static/js/main.1bb8dd19.chunk.js"
+  },
+  {
+    "revision": "9b1b9cc3bf76b1b915c8",
+    "url": "/portfolio/static/js/runtime-main.b5de2cbb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
